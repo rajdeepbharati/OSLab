1eb8e1ae3625cd536869ef97a5b7a2f75c8bdbf9
Rajdeep Bharati <rajdeep@macports.org>
2019-11-20 10:23:59 +0530

    Update max.sh

--- max.sh
+++ max.sh
@@ -5,8 +5,7 @@ read num2
 echo "Enter num3"
 read num3
 
-if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
-then
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]; then
     echo Greatest: $num1
 elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
 then
